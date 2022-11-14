101a103e6d4fe1fd8380415b8f4b657b6978efda
Izhal-Brown <informatika749@gmail.com>
2022-11-14 16:32:42 +0800

    mern-client

---
+++ src/components/BreadCrumb/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Breadcrumb } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
+
+function BreadCrumb({items}) {
+  const history = useHistory();
+
+  return (
+    <Breadcrumb>
+      {
+        items.map((item, i) => (
+          <Breadcrumb.Item 
+            key={i} 
+            active={i+1 === items.length} 
+            href="#" 
+            className='fw-bold text-danger'
+            onClick={ () => i + 1 !== items.length ? history.push(item.path): null }
+          >{item.label}</Breadcrumb.Item>
+        ))
+      }
+    </Breadcrumb>
+  )
+}
+
+BreadCrumb.propTypes = {
+  items: PropTypes.array.isRequired
+}
+
+export default BreadCrumb
+
